e7ceb39d9abfe97340b832e315760e27f3a5c454
Tyler Van Zyl <tylervanzyl@erm-mo-01-10.8.57.194.brighton.ac.uk>
2020-01-06 16:15:13 +0000

    Adding complete camera to master

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
 let planet;
 let sun;
-
-
 let force;
 let force2;
 
@@ -14,16 +12,30 @@ let planetSizeValue;
 
 const sunSize = document.querySelector("#sun-size");
 let sunSizeValue = 0;
+// Links sliders from HTML values as the camera position
+const xPos = document.querySelector("#x-position");
+let xPosValue;
+
+const yPos = document.querySelector("#y-position");
+let yPosValue;
+
+const zPos = document.querySelector("#z-position");
+let zPosValue;
 
 // Getting the height and width if the div the canvas is in
 const div = document.getElementById("sketch-canvas");
 let divWidth = div.offsetWidth;
 let divHeight = div.offsetHeight;
 
+ 
+
+// Event listener for Reset button for Camera HTML
+document.querySelector('#reset').addEventListener("click",resetCamera) 
 
 
 function setup() {
-
+    
+ 
     // Add the sketch to the div 
     const sketchCanvas = createCanvas(divWidth, divHeight, WEBGL);
     sketchCanvas.parent("sketch-canvas")
@@ -33,19 +45,24 @@ function setup() {
 
     planet = new Planet(planetSize.value);
 
-
-
-
-
 }
 
 
 function draw() {
+
+    // HTML values being assigned to variables
+    xPosValue = xPos.value;
+    yPosValue = yPos.value;
+    zPosValue = zPos.value;
+
+
     force = new p5.Vector();
 
     planetSizeValue = planetSize.value;
     sunSizeValue = sunSize.value;
-    background(30);
+    background(0);
+
+
 
 
     force = sun.attract(planet);
@@ -63,9 +80,26 @@ function draw() {
     sun.updateSize(sunSizeValue);
     sun.display();
     planet.display();
-
+    //changes Camera parameters for  X,Y,Z 
+    camera(xPosValue,yPosValue,constrain(zPosValue,sunSizeValue,1000), 0, 0, 0, 0,1, 0);
 }
+    
+    
+
+    
+   
+    
 // Makes sure canvas stays the same size when the browser is resized
 function windowResized() {
     createCanvas(divWidth, divHeight, WEBGL);
 }
+
+
+// 
+// Reset camera buttons
+function resetCamera(){
+    document.getElementById('x-position').value = 0;
+    document.getElementById('y-position').value = 0;
+    document.getElementById('z-position').value = 700;
+    
+    }
